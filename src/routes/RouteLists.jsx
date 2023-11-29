4e2cb80f9e720137fd7020864918aa248b61cd34
angga <68156108+anggavirgi@users.noreply.github.com>
2023-11-29 14:49:04 +0700

    update popup filter & tambah kelas

--- src/routes/RouteLists.jsx
+++ src/routes/RouteLists.jsx
@@ -4,15 +4,21 @@ import { DashboardAdmin } from "../pages/Admin/DashboardAdmin";
 import { ClassAdmin } from "../pages/Admin/ClassAdmin";
 import { Class } from "../pages/User/Class";
 import Detail from "../pages/User/Detail";
+import { Homepage } from "../pages/User/Homepage";
 
 export const RouteLists = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/admin" element={<DashboardAdmin />} />
-        <Route path="/kelolakelas" element={<ClassAdmin />} />
+        {/* USER */}
+        <Route path="/" element={<Homepage />} />
+        <Route path="/home" element={<Homepage />} />
         <Route path="/kelas" element={<Class />} />
         <Route path="/detail" element={<Detail />} />
+
+        {/* ADMIN */}
+        <Route path="/admin" element={<DashboardAdmin />} />
+        <Route path="/kelolakelas" element={<ClassAdmin />} />
       </Routes>
     </BrowserRouter>
   );
